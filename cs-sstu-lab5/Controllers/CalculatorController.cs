51afb55c65dddb7d92626a908d9ed592a62b4b60
artem <artem@DESKTOP-2F12TNQ>
2022-05-06 16:22:06 +0400

    Добавьте файлы проекта.

---
+++ cs-sstu-lab5/Controllers/CalculatorController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using cs_sstu_lab5.Models;
+
+namespace cs_sstu_lab5.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CalculatorModel calculatorModel)
+        {
+            if (!ModelState.IsValid) return View();
+
+            switch(calculatorModel.Operation)
+            {
+                case CalculatorModel.OperationType.sum: 
+                    calculatorModel.Result = calculatorModel.A + calculatorModel.B; break;
+                case CalculatorModel.OperationType.substract: 
+                    calculatorModel.Result = calculatorModel.A - calculatorModel.B; break;
+                case CalculatorModel.OperationType.divide: 
+                    calculatorModel.Result = calculatorModel.A / calculatorModel.B; break;
+                case CalculatorModel.OperationType.multiply: 
+                    calculatorModel.Result = calculatorModel.A * calculatorModel.B; break;
+            }
+
+            return View(calculatorModel);
+        }
+    }
+}
---
+++ cs-sstu-lab5/Models/CalculatorModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cs_sstu_lab5.Models
+{
+    public class CalculatorModel
+    {
+        [Display(Name = "First Number")]
+        public double A { get; set; }
+        [Display(Name = "Second Number")]
+        public double B { get; set; }
+        public OperationType Operation { get; set; }
+        public double Result { get; set; }
+
+        public enum OperationType
+        {
+            sum,
+            substract,
+            divide,
+            multiply
+        }
+    }
+}
