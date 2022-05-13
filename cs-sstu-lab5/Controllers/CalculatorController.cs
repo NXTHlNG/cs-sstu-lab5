de74524aa828dba1b465d5a301bf293343313d75
NXTHING <nxthing.az@gmail.com>
2022-05-13 20:20:56 +0400

    added partial view and ajax support

--- cs-sstu-lab5/Controllers/CalculatorController.cs
+++ cs-sstu-lab5/Controllers/CalculatorController.cs
@@ -11,38 +11,7 @@ namespace cs_sstu_lab5.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(CalculatorModel calculatorModel)
-        {
-            double a, b;
-            
-            if (double.TryParse(calculatorModel.A, out a) && double.TryParse(calculatorModel.B, out b))
-            {
-                switch (calculatorModel.Operation)
-                {
-                    case CalculatorModel.OperationType.sum:
-                        calculatorModel.Result = (a + b).ToString(); break;
-
-                    case CalculatorModel.OperationType.substract:
-                        calculatorModel.Result = (a - b).ToString(); break;
-
-                    case CalculatorModel.OperationType.divide:
-                        if (b == 0) calculatorModel.Result = "Divide by zero";
-                        else calculatorModel.Result = (a / b).ToString(); break;
-
-                    case CalculatorModel.OperationType.multiply:
-                        calculatorModel.Result = (a * b).ToString(); break;
-                }
-            }
-            else
-            {
-                calculatorModel.Result = "Not a number";
-            }
-
-            return View(calculatorModel);
-        }
-
-        [HttpPost]
-        public string Calculate([FromBody] CalculatorModel calculatorModel)
+        public string Calculate(CalculatorModel calculatorModel)
         {
             double a, b, result = 0;
 
